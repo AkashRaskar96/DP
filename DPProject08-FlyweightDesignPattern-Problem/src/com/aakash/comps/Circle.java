469e084f89d5b4e54979d1e90dd59c45e32c58f7
akash <akashraskar.adt@gmail.com>
2023-05-08 19:39:05 +0530

    FlyWeight DP

---
+++ DPProject08-FlyweightDesignPattern-Problem/src/com/aakash/comps/Circle.java
@@ -0,0 +1,11 @@
+package com.aakash.comps;
+
+public class Circle implements Shape {
+
+	@Override
+	public void draw(int arg, String fillColor, String lineStyle) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DPProject08-FlyweightDesignPattern-Problem/src/com/aakash/comps/Shape.java
@@ -0,0 +1,5 @@
+package com.aakash.comps;
+
+public interface Shape {
+	public void draw(int arg, String fillColor, String lineStyle);
+}
